b89c65560f77e6de77e4aae15afa36d9b228ba39
giovanni <giovanni.ggoncalves@outlook.com>
2025-08-26 11:06:49 -0300

    feat: create divisão and multiplicação

--- __tests__/calculadora.test.js
+++ __tests__/calculadora.test.js
@@ -1,4 +1,4 @@
-import { somar, subtrair } from '../src/calculadora.js';
+import { somar, subtrair, multiplicar, dividir } from '../src/calculadora.js';
 
 describe("Calculadora", () => {
     test("Deve somar dois números corretamente", () => {
@@ -7,10 +7,29 @@ describe("Calculadora", () => {
        const resultado = somar(a, b);
        expect(resultado).toBe(5);
     });
+
 test("Deve subtrair dois números corretamente", () => {
        const a = 13;
        const b = 3;
        const resultado = subtrair(a, b);
        expect(resultado).toBe(10);
     });
+
+test("Deve multiplicar dois números corretamente", () => {
+       const a = 10;
+       const b = 4;
+       const resultado = multiplicar(a, b);
+       expect(resultado).toBe(40);
+   });
+
+   test("Deve dividir dois números corretamente", () => {
+       const a = 10;
+       const b = 5;
+       const resultado = dividir(a, b);
+       expect(resultado).toBe(2);
+   });
+
+   test("Deve lançar um erro ao dividir por zero", () => {
+       expect(() => dividir(10, 0)).toThrow("Divisão por zero não permitida");
+   });
 })
